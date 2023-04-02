80f951d61fc93d9a60a6c57bb1a713360716b0fd
Gandls42anth <70642446+Gandls42anth@users.noreply.github.com>
2023-04-02 18:42:19 -0400

    http to htttps

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,19 +1,24 @@
 const requiresLogin = (req,res,next) => {
+    console.log("requiresLogin");
     if(!req.session.account) {
+        console.log("found no account with this session, redirecting to index");
         return res.redirect('/');
     }
     return next();
 }
 
 const requiresLogout = (req,res,next) => {
+    console.log("requires logout");
     if(req.session.account) {
+        console.log("found a session with an account, redirecting to maker");
         return res.redirect('/maker');
     }
     return next();
 }
 
-const requireSecure = (req,res,next) => {
-    if(req.headers['x-forwarded-proto'] !== 'http') {
+const requiresSecure = (req,res,next) => {
+    console.log("in requires secure");
+    if(req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect(`http://${req.hostname}${req.url}`);
     }
     return next();
@@ -26,7 +31,7 @@ module.exports.requiresLogin = requiresLogin;
 module.exports.requiresLogout = requiresLogout;
 
 if(process.env.NODE_ENV === 'production'){
-    module.exports.requireSecure = requireSecure;
+    module.exports.requiresSecure = requiresSecure;
 }else{
-    module.exports.requireSecure = bypassSecure;
+    module.exports.requiresSecure = bypassSecure;
 }
